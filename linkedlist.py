c6dd34022148241e1bce2e51064646b893f4b42e
Collin M <wingedillidan@gmail.com>
2015-08-25 22:32:04 -0700

    Well... reverse() works...

--- linkedlist.py
+++ linkedlist.py
@@ -19,10 +19,43 @@ class LinkedListNode(object):
         while nxt is not None:
             nxt = nxt._displaynode()
 
+    def setnxt(self, nxt):
+        self.nxt = nxt
+        return nxt
+
+    def reverse(self):
+        result = None
+        ondeck = None
+
+        while self.nxt is not None:
+            nxt = self._displaynode()
+
+            if nxt.nxt is not None:
+                while nxt.nxt.nxt is not None:
+                    nxt = nxt._displaynode()
+            else:
+                break
+
+            if result is None:
+                result = LinkedListNode(nxt.nxt.val)
+                ondeck = result
+            else:
+                ondeck = ondeck.setnxt(nxt.nxt)
+
+            nxt.nxt = None
+
+        ondeck = ondeck.setnxt(self.nxt)
+        ondeck = ondeck.setnxt(LinkedListNode(self.val))
+
+        return result
+
+
 ll = LinkedListNode(1)
 nxt = ll.add(2)
 
 for i in xrange(3, 6):
     nxt = nxt.add(i)
 
+ll.display()
+ll = ll.reverse()
 ll.display()
