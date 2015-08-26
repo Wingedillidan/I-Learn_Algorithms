0568ae715457a0b97ce93b8d541a2885923e3bd8
Collin M <wingedillidan@gmail.com>
2015-08-25 20:24:47 -0700

    First attempt at instantiation...

--- linkedlist.py
+++ linkedlist.py
@@ -6,3 +6,15 @@ class LinkedListNode(object):
 
     def add(self, val):
         self.nxt = LinkedListNode(val)
+
+        return self.nxt
+
+i = raw_input('> ')
+ll = LinkedListNode()
+last = ll
+
+for j in xrange(0, int(i)):
+    last = last.add(j)
+
+print ll.val
+print ll.nxt.nxt.nxt.nxt.val
