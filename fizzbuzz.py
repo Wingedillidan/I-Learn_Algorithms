9343ab3f1f744253632646363a8b0d3b2ad5eef5
Collin M <wingedillidan@gmail.com>
2015-08-25 18:13:59 -0700

    fizzbuzz definition

---
+++ fizzbuzz.py
@@ -0,0 +1,7 @@
+"""
+Receive a number on stdin. For every number from 1 to the number received
+(inclusive), print "fizz" if the number is divisible by 3, "buzz" if it's
+divisible by 5, "fizzbuzz" if it's divisible by both, or the number if it's
+not divisible by either.
+"""
+
