19a5aa44bbe3ed3145457bca978cbbc909be10d5
Collin M <wingedillidan@gmail.com>
2015-09-12 14:22:18 -0400

    attempt 1

--- collaks.py
+++ collaks.py
@@ -3,18 +3,26 @@ class Conjecture(object):
     def __init__(self, number):
         self.number = abs(number)
         self.maths = 0
+        self.whereivebeen = {}
 
     def conject(self):
         whereimat = 0
 
         for i in xrange(1, self.number+1):
             whereimat = i
+            localmaths = 0
 
             while whereimat is not 1:
-                self.maths += 1
+                if self.whereivebeen.get(whereimat, None):
+                    self.maths += self.whereivebeen[whereimat]
+                    break
+                else:
+                    localmaths += 1
+
                 whereimat = self.calc(whereimat)
 
-            self.maths += 1
+            self.whereivebeen[i] = localmaths
+            self.maths += localmaths + 1
 
     def calc(self, num):
         if num % 2 == 0:
@@ -29,4 +37,5 @@ difficulty = raw_input('> ')
 mathamajigz = Conjecture(int(difficulty))
 mathamajigz.conject()
 
+print mathamajigz.whereivebeen
 print 'maths:', mathamajigz.maths
